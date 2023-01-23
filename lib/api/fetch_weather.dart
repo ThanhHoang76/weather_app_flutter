871d5e49af75fb3c8f4b7d46b10bedc3b3313577
ThanhHoang76 <thanhhoangit76@gmail.com>
2023-01-24 00:34:23 +0700

    add statuscode

--- lib/api/fetch_weather.dart
+++ lib/api/fetch_weather.dart
@@ -4,7 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app_flutter/model/weather_data_current.dart';
 import 'package:weather_app_flutter/model/weather_data_daily.dart';
 import 'package:weather_app_flutter/model/weather_data_hourly.dart';
-
+import 'package:get/get.dart';
+import 'package:weather_app_flutter/screens/no_data.dart';
 import '../ultis/api_url.dart';
 
 class FetchWeatherApi {
@@ -28,6 +29,10 @@ class FetchWeatherApi {
       WeatherDataHourly.fromJson(jsonString),
       WeatherDataDaily.fromJson(jsonString),
     );
+    if(response.statusCode == 404){
+      Get.to(NoDataScreen());
+    }
     return weatherData!;
+
   }
 }
